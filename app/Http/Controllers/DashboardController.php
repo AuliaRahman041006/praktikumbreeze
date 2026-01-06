a9714399a8439e9290e09127f86d9036279abe01
AuliaRahman041006 <quarcom041006@gmail.com>
2026-01-06 16:44:14 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard');
+    }
 }
